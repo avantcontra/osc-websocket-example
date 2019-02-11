9622c6cab79da03f64a77a17257e90126edcba35
Contra <dadadaluo@gmail.com>
2019-02-11 18:39:17 +0800

    p5js add a Host IP input.

--- p5js-client/sketch.js
+++ p5js-client/sketch.js
@@ -1,51 +1,70 @@
 
-var host = 'localhost:12345';
+var currentHost = 'no input';
 var oscWebSocket;
 var statusMessage = 'not connected';
 var receivedMessage = '';
 
-let input;
+let inputMessage;
+let inputHost;
 
 function setup() {
   createCanvas(400, 400);
 
   let button = createButton('connect');
-  button.position(250, 60);
+  button.position(250, 80);
   button.mousePressed(onConnectClick);
 
-  input = createInput();
-  input.position(80, 270);
+  inputMessage = createInput();
+  inputMessage.position(50, 270);
+
+  inputHost = createInput();
+  inputHost.position(50, 90);
 
   let buttonSend = createButton('send');
   buttonSend.position(250, 260);
   buttonSend.mousePressed(onSendClick);
 
-  textSize(20);
-  // connect to WebSocket server:
-  oscWebSocket = new osc.WebSocketPort({
-    url: "ws://localhost:12345",
-    metadata: true
-  });
-
-  oscWebSocket.on("ready", onSocketOpen);
-  oscWebSocket.on("message", onSocketMessage);
+  textSize(16);
 }
 
 function draw() {
   background('#999999');
   fill(255);
-  text('SERVER HOST:  ' + host, 50, 40);
-  text(statusMessage, 50, 80);
-  text(receivedMessage, 50, 150);
+  text('host example: 192.168.1.23:12345', 50, 40);
+  text('current host:  ' + currentHost, 50, 70);
+  text(statusMessage, 50, 150);
+  text(receivedMessage, 50, 190);
+  text('input some message:', 50, 250);
 }
 
 function onConnectClick() {
-  oscWebSocket.open();
+  currentHost = inputHost.value();
+  inputHost.value('');
+
+  // connect to WebSocket server:
+  try {
+    oscWebSocket = new osc.WebSocketPort({
+      url: "ws://" + currentHost,
+      metadata: true
+    });
+  
+    oscWebSocket.on("ready", onSocketOpen);
+    oscWebSocket.on("message", onSocketMessage);
+    oscWebSocket.on("error", function(e){
+      print(e.message);
+    });
+  
+    oscWebSocket.open();
+  } catch(e) {
+    print(e);
+    statusMessage = e;
+  }
+  
 }
 
 function onSendClick() {
-  const msg = input.value();
-  input.value('');
+  const msg = inputMessage.value();
+  inputMessage.value('');
 
   // send the OSC message to server. (osc.js will convert it to binary packet):
   oscWebSocket.send({
@@ -75,5 +94,4 @@ function onSocketMessage(message) {
       receivedMessage += ', value: ' + message.args[0].value;
     }
   }
-
 }
